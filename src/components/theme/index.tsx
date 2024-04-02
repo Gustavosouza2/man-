a9d2bc4dcd848009dc2770ebe990d0af02f29f36
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-04-02 00:02:38 -0300

    feat:creating arquvi theme to customize application

---
+++ src/components/theme/index.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    main: {
+      dark: {
+        100: '#16181B',
+      },
+      cyan: {
+        100: '#00FFA3',
+      },
+    },
+  },
+})
