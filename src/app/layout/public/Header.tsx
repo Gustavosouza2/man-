93204a49eedc80c8a1e5cc34fd730dd890b41d64
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-04-07 23:28:46 -0300

    wip:add menu hamburguer and MobileBreakPoint

--- src/app/layout/public/Header.tsx
+++ src/app/layout/public/Header.tsx
@@ -1,8 +1,21 @@
 'use client'
 
-import { Flex, Text } from '@chakra-ui/react'
+import { IoMenu, IoPeople } from 'react-icons/io5'
+
+import {
+  IconButton,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+  Flex,
+  Menu,
+} from '@chakra-ui/react'
+import { useMobileBreakPoint } from '@/ultils/MobileBreakPoint'
 
 export const Header = () => {
+  const isMobile = useMobileBreakPoint()
+
   return (
     <Flex
       borderBottom="1px solid #73777D"
@@ -12,7 +25,30 @@ export const Header = () => {
       height="5rem"
       width="100%"
     >
-      <Flex mr="5.9rem" gap="1.9rem">
+      {!isMobile && (
+        <Flex mr="1.5rem">
+          <Menu>
+            <MenuButton
+              icon={<IoMenu color="#FFF" fontSize="2.5rem" />}
+              _active={{ bgColor: 'transparent' }}
+              _hover={{ bgColor: 'transparent' }}
+              aria-label="Options"
+              as={IconButton}
+              variant="ghost"
+            />
+            <MenuList>
+              <MenuItem
+                icon={<IoPeople color="#000" fontSize="1.5rem" />}
+                fontFamily="var(--font-inter)"
+                fontWeight={600}
+              >
+                Clientes
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      )}
+      <Flex mr="5.9rem" gap="1.9rem" hidden={!isMobile}>
         <Text textColor="main.cyan.100" cursor="pointer" fontSize="1.1rem">
           clientes
         </Text>
