6d4b354abfcfd7110a6266f684a7f5fcf8e7e008
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-04-14 22:47:24 -0300

    add new vars colors in theme file

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -5,8 +5,15 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   colors: {
     main: {
+      green: {
+        100: '#0E492D',
+      },
+      beige: {
+        100: '#ECD0B3',
+      },
       dark: {
-        100: '#16181B',
+        100: '#282828',
+        200: '#191815',
       },
       cyan: {
         100: '#00FFA3',
