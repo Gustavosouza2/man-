4dee64ba5afde7852f26f19a4474e1cb525a523e
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-05-04 22:55:42 -0300

    feat:adding styles and variants to the component button

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -6,10 +6,10 @@ export const theme = extendTheme({
   colors: {
     main: {
       green: {
-        100: '#0E492D',
+        100: '#00754A',
       },
       beige: {
-        100: '#ECD0B3',
+        100: '#DFD5B2',
       },
       dark: {
         100: '#282828',
@@ -17,6 +17,38 @@ export const theme = extendTheme({
       },
       cyan: {
         100: '#00FFA3',
+      },
+    },
+  },
+
+  components: {
+    Button: {
+      sizes: {
+        sm: {
+          height: '2.625rem',
+          width: '7.5rem',
+        },
+      },
+
+      variants: {
+        primary: {
+          borderRadius: '1.188rem',
+          _hover: { bg: '#186642' },
+          bg: 'main.green.100',
+          height: '3.125rem',
+          width: '9.688rem',
+        },
+
+        outline: {
+          _hover: { bg: '#00754A', color: '#fff' },
+          borderRadius: '1.188rem',
+          borderWidth: '0.063rem',
+          borderColor: '#00754A',
+          borderStyle: 'solid',
+          height: '3.125rem',
+          bg: 'transparent',
+          width: '9.688rem',
+        },
       },
     },
   },
