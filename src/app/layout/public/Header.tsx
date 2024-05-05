48af878fc45a97ba59d446923c1bd06fb5259f4d
Gustavosouza2 <gustavoleonsouza@gmail.com>
2024-05-04 22:54:14 -0300

    wip:new properties and new titles

--- src/app/layout/public/Header.tsx
+++ src/app/layout/public/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { FaRegCircleUser } from 'react-icons/fa6'
 import { IoMenu, IoPeople } from 'react-icons/io5'
 import {
   IconButton,
@@ -9,38 +10,60 @@ import {
   Text,
   Flex,
   Menu,
+  Box,
 } from '@chakra-ui/react'
 
 import { useMobileBreakPoint } from '@/utils/MobileBreakPoint'
-import { CasaLogo } from '../assets/CasaLogo'
+import { PropsWithChildren } from 'react'
 import Link from 'next/link'
+
+const TitleHeader = ({ children }: PropsWithChildren) => {
+  return (
+    <Text
+      _hover={{ transform: 'scale(1.10)', textDecoration: 'none' }}
+      transition="transform 300ms ease-in-out"
+      fontFamily="var(--font-montagu_slab)"
+      textColor="#000000"
+      fontSize="0.8rem"
+      cursor="pointer"
+      fontWeight="500"
+    >
+      {children}
+    </Text>
+  )
+}
 
 export const Header = () => {
   const isMobile = useMobileBreakPoint()
 
   return (
     <Flex
-      borderBottom="1px solid #ECD0B3"
+      justifyContent="space-between"
       bg="transparent"
-      height="5rem"
+      height="7rem"
       width="100vw"
     >
-      <CasaLogo />
       {isMobile && (
-        <Flex alignItems="center" justify="flex-end" w="100%" mr="2rem">
+        <Flex
+          justify={['flex-end', 'center']}
+          mr={['1.2rem', '0rem']}
+          alignItems="center"
+          w="100%"
+        >
           <Menu>
             <MenuButton
-              icon={<IoMenu color="#ECD0B3" fontSize="2.5rem" />}
+              icon={<IoMenu color="#000000" fontSize="2.5rem" />}
               _active={{ bgColor: 'transparent' }}
               _hover={{ bgColor: 'transparent' }}
               aria-label="Options"
               as={IconButton}
               variant="ghost"
             />
-            <MenuList>
+            <MenuList bg="main.beige.100">
               <MenuItem
                 icon={<IoPeople color="#000" fontSize="1.5rem" />}
-                fontFamily="var(--font-inter)"
+                fontFamily="var(--font-montagu_slab)"
+                bg="main.beige.100"
                 fontWeight={600}
               >
                 <Link href="./">Clientes</Link>
@@ -51,15 +74,21 @@ export const Header = () => {
       )}
       <Flex
         hidden={!!isMobile}
+        position="relative"
         alignItems="center"
-        justify="flex-end"
+        justify="center"
         gap="1.9rem"
-        mr="7rem"
         w="100%"
       >
-        <Text textColor="main.beige.100" cursor="pointer" fontSize="1.3rem">
-          Clientes
-        </Text>
+        <TitleHeader>HOME</TitleHeader>
+        <TitleHeader>CLIENTES</TitleHeader>
+        <TitleHeader>PAY</TitleHeader>
+        <TitleHeader>ORDER</TitleHeader>
+        <TitleHeader>STORE</TitleHeader>
+
+        <Box justifyContent="end" position="absolute" right={60}>
+          <FaRegCircleUser />
+        </Box>
       </Flex>
     </Flex>
   )
